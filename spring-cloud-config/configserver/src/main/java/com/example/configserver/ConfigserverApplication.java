f41f402d8f8cc1d284bc65f5f45a419326da2b52
yangfan <2585928225@qq.com>
2018-04-13 09:57:20 +0800

    config read local file

--- spring-cloud-config/configserver/src/main/java/com/example/configserver/ConfigserverApplication.java
+++ spring-cloud-config/configserver/src/main/java/com/example/configserver/ConfigserverApplication.java
@@ -1,14 +1,16 @@
 package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+@EnableAutoConfiguration
+public class ConfigserverApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
 }
